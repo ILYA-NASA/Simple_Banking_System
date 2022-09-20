e973849780935f1504d8022e8f937b7718da0e18
ILYA.NASA <il.nasa@outlook.com>
2022-09-20 11:32:03 +0300

    problem of the day

--- Topics/Scanning the input/Hiring a chef/src/Main.java
+++ Topics/Scanning the input/Hiring a chef/src/Main.java
@@ -3,6 +3,15 @@ import java.util.Scanner;
 class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        // start coding here
+        String word1 = scanner.next();
+        String word2 = scanner.next();
+        String word3 = scanner.next();
+        System.out.print("The form for ");
+        System.out.print(word1);
+        System.out.print(" is completed. We will contact you if we need a chef who cooks ");
+        System.out.print(word3);
+        System.out.print(" dishes and has ");
+        System.out.print(word2);
+        System.out.print(" years of experience.");
     }
 }
