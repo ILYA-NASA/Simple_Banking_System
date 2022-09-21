7ccd899ad83713f2a50aa06e101420145e61b9ba
ILYA.NASA <il.nasa@outlook.com>
2022-09-21 11:20:36 +0300

    problem of the day is done

---
+++ Topics/Iterating over arrays/Alphabetical order/src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+
+    }
+
+    public String[] createArray () {
+        Scanner sc = new Scanner(System.in);
+        String[] array = new String[];
+
+    }
+}
