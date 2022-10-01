f521d23fbc44101531b232dc43f94415a9ac8211
ILYA.NASA <il.nasa@outlook.com>
2022-10-01 15:13:43 +0300

    added user choose and controller draft

--- Simple Banking System/task/src/banking/Menu.java
+++ Simple Banking System/task/src/banking/Menu.java
@@ -1,13 +1,22 @@
 package banking;
 
+import banking.account.CreateAccount;
+
 import java.util.Scanner;
 
 public class Menu {
-    public static void startsMenu() {
+    public static String startsMenu() {
         Scanner sc = new Scanner(System.in);
         System.out.println("1. Create an account\n" +
                 "2. Log into account\n" +
                 "0. Exit");
+        String cardInfo = new String();
         int costChoose = sc.nextInt();
+        if (costChoose == 1) {
+            cardInfo = CreateAccount.generateCardNum() + "\n" + CreateAccount.generatePinCode();
+        } else if (costChoose == 2) {
+            //TODO
+        }
+        return cardInfo;
     }
 }
