57ec00c5c0e3f0acb668429b4cf34fe8a1224a09
ILYA.NASA <il.nasa@outlook.com>
2022-10-03 20:20:34 +0300

    output according to task

--- Simple Banking System/task/src/banking/Menu.java
+++ Simple Banking System/task/src/banking/Menu.java
@@ -8,38 +8,27 @@ import java.util.Scanner;
 public class Menu {
     public static String[] startsMenu(String card, String pin) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("1. Create an account\n" +
+        System.out.println("\n1. Create an account\n" +
                 "2. Log into account\n" +
                 "0. Exit");
         int costChoose = sc.nextInt();
         if (costChoose == 1) {
             String cardNum = CreateAccount.generateCardNum();
             String pinCode = CreateAccount.generatePinCode();
-            System.out.println("Your card has been created\nYour card number:\n"
+            System.out.println("\nYour card has been created\nYour card number:\n"
                     + cardNum
                     + "\nYour card PIN:\n" + pinCode);
             startsMenu(cardNum, pinCode);
         } else if (costChoose == 2) {
-            System.out.println("Enter your card number:");
+            System.out.println("\nEnter your card number:");
             String cardNum = sc.next();
             System.out.println("Enter your PIN:");
             String pinCode = sc.next();
             if (cardNum.equals(card) && pinCode.contentEquals(pin)) {
-                System.out.println("You have successfully logged in!");
-                System.out.println("1. Balance\n" +
-                        "2. Log out\n" +
-                        "0. Exit");
-                int choose = sc.nextInt();
-                if (choose == 1) {
-                    System.out.println("Balance: " + UsedAccount.balance());
-                } else if (choose == 2) {
-                    System.out.println("You have successfully logged out!");
-                    startsMenu(cardNum, pinCode);
-                } else {
-                    System.out.println("Bye!");
-                }
+                System.out.println("\nYou have successfully logged in!");
+                UsedAccount.accountMenu(cardNum, pinCode);
             } else {
-                System.out.println("Wrong card number or PIN!");
+                System.out.println("\nWrong card number or PIN!");
                 startsMenu(card, pin);
             }
         }
--- Simple Banking System/task/src/banking/account/CreateAccount.java
+++ Simple Banking System/task/src/banking/account/CreateAccount.java
@@ -6,14 +6,14 @@ public class CreateAccount {
     public static String generateCardNum() {
         String bin = "400000";
         Random rnd = new Random();
-        String accountIdentifier = String.valueOf(rnd.nextInt(000000000, 999999999));
+        String accountIdentifier = String.valueOf(rnd.nextInt(100000000, 999999999));
         String checksum = "9"; //will be Luhn algorithm
         return bin + accountIdentifier + checksum;
     }
 
     public static String generatePinCode() {
         Random rnd = new Random();
-        int pin = rnd.nextInt(0000, 9999);
+        int pin = rnd.nextInt(1000, 9999);
         return String.valueOf(pin);
     }
 }
