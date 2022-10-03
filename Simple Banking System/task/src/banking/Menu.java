3ce75cdad2d30d5c78b045ead936edb533819a66
ILYA.NASA <il.nasa@outlook.com>
2022-10-03 16:36:57 +0300

    added array for storage data user card

--- Simple Banking System/task/src/banking/Menu.java
+++ Simple Banking System/task/src/banking/Menu.java
@@ -2,20 +2,26 @@ package banking;
 
 import banking.account.CreateAccount;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Menu {
-    public static String startsMenu() {
+    public static String[] startsMenu() {
         Scanner sc = new Scanner(System.in);
         System.out.println("1. Create an account\n" +
                 "2. Log into account\n" +
                 "0. Exit");
-        String cardInfo = new String();
+        final int cardInfoLength = 2;
+        String[] cardInfo = new String[cardInfoLength];
         int costChoose = sc.nextInt();
         if (costChoose == 1) {
-            cardInfo = "Your card has been created\nYour card number:\n"
-                    + CreateAccount.generateCardNum()
-                    + "\nYour card PIN:\n" + CreateAccount.generatePinCode();
+            String cardNum = CreateAccount.generateCardNum();
+            String pinCode = CreateAccount.generatePinCode();
+            System.out.println("Your card has been created\nYour card number:\n"
+                              + cardNum
+                              + "\nYour card PIN:\n" + pinCode);
+            cardInfo[0] = cardNum;
+            cardInfo[1] = pinCode;
         } else if (costChoose == 2) {
             //TODO
         }
