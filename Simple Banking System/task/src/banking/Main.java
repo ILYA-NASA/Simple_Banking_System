8f005dbb00d8513d75d25a81a731dc0c8068cbe6
ILYA.NASA <il.nasa@outlook.com>
2022-10-03 18:37:49 +0300

    sample run

--- Simple Banking System/task/src/banking/Main.java
+++ Simple Banking System/task/src/banking/Main.java
@@ -1,9 +1,7 @@
 package banking;
 
-import java.util.Arrays;
-
 public class Main {
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(Menu.startsMenu("", "")));
+        Menu.startsMenu("", "");
     }
 }
