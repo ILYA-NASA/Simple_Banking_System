53c3f375ea92bcaf8c95c2e559022fe26440100b
ILYA.NASA <il.nasa@outlook.com>
2022-10-03 20:18:13 +0300

    added accountMenu method (contains recursion)

--- Simple Banking System/task/src/banking/account/UsedAccount.java
+++ Simple Banking System/task/src/banking/account/UsedAccount.java
@@ -1,8 +1,29 @@
 package banking.account;
 
+import banking.Menu;
+
+import java.util.Scanner;
+
 public class UsedAccount {
-    public static int balance () {
-        return 0;
+    public static void accountMenu(String cardNum, String pinCode) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("\n1. Balance\n" +
+                "2. Log out\n" +
+                "0. Exit");
+        int choose = sc.nextInt();
+        if (choose == 1) {
+            balance();
+            accountMenu(cardNum, pinCode);
+        } else if (choose == 2) {
+            System.out.println("\nYou have successfully logged out!");
+            Menu.startsMenu(cardNum, pinCode);
+        } else {
+            System.out.println("\nBye!");
+        }
+    }
+
+    public static void balance() {
+        System.out.println("\nBalance: " + 0);
     }
 
 }
