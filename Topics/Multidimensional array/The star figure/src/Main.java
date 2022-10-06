d8909338a8e01c9d03786802c845040959e2df1c
ILYA.NASA <il.nasa@outlook.com>
2022-10-06 22:27:58 +0300

    added create and print methods

--- Topics/Multidimensional array/The star figure/src/Main.java
+++ Topics/Multidimensional array/The star figure/src/Main.java
@@ -1,30 +1,34 @@
 import java.util.Scanner;
 
 class Main {
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int length = sc.nextInt();
-        String[][] arr = new String[length][length];
+        printArray(createStar(sc.nextInt()));
+    }
+
+    public static String[][] createStar(int size) {
+        String[][] arr = new String[size][size];
         int k = 0;
         int d = arr.length - 1;
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr.length; j++) {
                 arr[i][j] = ".";
-                if (j == arr.length / 2) {
+                if (j == arr.length / 2 || i == j) {
                     arr[i][j] = "*";
                 }
                 if (i == arr.length / 2) {
                     arr[i][k] = "*";
                     k++;
                 }
-                if (i == j) {
-                    arr[i][j] = "*";
-                }
             }
             arr[i][d] = "*";
             d--;
         }
+        return arr;
+    }
 
+    public static void printArray(String[][] arr) {
         for (String[] str : arr) {
             for (String s : str) {
                 System.out.print(s + " ");
