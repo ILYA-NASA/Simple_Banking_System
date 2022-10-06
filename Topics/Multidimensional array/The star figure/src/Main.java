491b914cc5f18bf2f56e7b3705df794f67b7f549
ILYA.NASA <il.nasa@outlook.com>
2022-10-06 22:18:51 +0300

    added foreach loop to print array

--- Topics/Multidimensional array/The star figure/src/Main.java
+++ Topics/Multidimensional array/The star figure/src/Main.java
@@ -25,9 +25,9 @@ class Main {
             d--;
         }
 
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr[i].length; j++) {
-                System.out.print(arr[i][j] + " ");
+        for (String[] str : arr) {
+            for (String s : str) {
+                System.out.print(s + " ");
             }
             System.out.println();
         }
