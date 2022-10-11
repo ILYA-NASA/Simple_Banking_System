cbcc5fe2fa82ee16ae3f951077defbf4c7612227
ILYA.NASA <il.nasa@outlook.com>
2022-10-11 20:51:18 +0300

    created checkSum method draft (including documentation)

--- Simple Banking System/task/src/banking/account/CreateAccount.java
+++ Simple Banking System/task/src/banking/account/CreateAccount.java
@@ -7,7 +7,7 @@ public class CreateAccount {
         String bin = "400000";
         Random rnd = new Random();
         String accountIdentifier = String.valueOf(rnd.nextInt(100000000, 999999999));
-        String checksum = "9"; //will be Luhn algorithm
+        String checksum = checkSum(bin, accountIdentifier);
         return bin + accountIdentifier + checksum;
     }
 
@@ -15,5 +15,22 @@ public class CreateAccount {
         Random rnd = new Random();
         int pin = rnd.nextInt(1000, 9999);
         return String.valueOf(pin);
+    }
+
+    /**
+     * method generate last number in card for checks in Luhn algorithm
+     */
+    public static String checkSum(String bin, String accountIden) {
+        String numCard = bin + accountIden;
+        int sum = 0;
+        for (int i = 0; i < numCard.length(); i++) {
+            sum += Integer.parseInt(numCard.substring(i, i + 1));
+        }
+        int count = 0;
+        while (sum % 10 != 0) {
+            sum++;
+            count++;
+        }
+        return String.valueOf(count);
     }
 }
