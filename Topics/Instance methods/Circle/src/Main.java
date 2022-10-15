03cfa063c7387fb6642e9a73cd285998df91dc55
ILYA.NASA <il.nasa@outlook.com>
2022-10-15 14:38:10 +0300

    task named Circle is done

---
+++ Topics/Instance methods/Circle/src/Main.java
@@ -0,0 +1,13 @@
+
+class Circle {
+    double radius;
+
+    // write methods here
+    public double getArea() {
+        return Math.PI * radius * radius;
+    }
+
+    public double getLength() {
+        return 2 * Math.PI * radius;
+    }
+}
