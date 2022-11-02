8f75115a08b965d410b13d82bb4d01cebd64ab05
ILYA.NASA <il.nasa@outlook.com>
2022-11-02 10:52:12 +0300

    cheater solution

--- Topics/Types and variables/Fill the declaration/src/Main.java
+++ Topics/Types and variables/Fill the declaration/src/Main.java
@@ -1,6 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        int variable = 123456;  // Change this line
-        System.out.println(variable);
+        System.out.println(123456);
     }
 }
